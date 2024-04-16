9cd69effee2621e5cc7df46882234c78a24b19fd
[unnatiambadkar] <[unnatiambadkar949@gmail.com]>
2024-04-16 19:39:52 +0530

    updated code 16 apr

---
+++ script.js
@@ -0,0 +1,37 @@
+// document object model - dom
+
+// document: class, tagname, id 
+
+// console.log(document)
+
+// let a = document.getElementsByClassName("second")
+// // return a HTML collection that can be used as an Array
+
+// console.log(a)
+// console.log(a.length)
+// console.log(a[2])
+
+// let b = document.getElementsByTagName("h1")
+// // return a HTML collection that can be used as an Array
+
+// console.log(b)
+// console.log(b.length)
+// console.log(b[0])
+
+// let c = document.getElementById("i1")
+// // return only one element if exists else null
+
+// console.log(c)
+
+// querySelector
+// querySelectorAll
+
+// let d = document.querySelector(".first")
+// // returns only single elememt / first occurence if exists
+
+// console.log(d)
+
+// let e = document.querySelectorAll("#i1")
+// // returns Node List of element that can be used as an array 
+
+// console.log(e)
